8f909b7ac171dd916d7b7bd1c67c3abe202d720e
zjw <zjw194744>
2022-04-07 00:08:29 +0800

    validation学习代码注释添加

--- src/main/java/com/validator/base/domain/Basics.java
+++ src/main/java/com/validator/base/domain/Basics.java
@@ -16,38 +16,49 @@ import java.util.Date;
 public class Basics {
 
     /** 主键 */
+    //被注释的元素必须不为 null
     @NotNull  //(message = "主键别空着")
     private String id;
 
     /** 姓名 */
+    //被注释的字符串（!=null && !""）、集合（size>0）
     @NotEmpty
     private String name;
 
     /** 证件号 */
+    //被注释的字符串（!=null && !"" && !"   "）
     @NotBlank
+    //被注释的元素的大小必须在指定范围内
     @Size(max = 18)
     private String idCard;
 
     /** 年龄 */
+    //被注释的元素必须在合适的范围内
     @Range(min = 20, max = 60)
     private Integer age;
 
+    /** 出生日期 */
+    //被注释的元素必须是一个过去的日期
     @Past
     private Date birthday;
 
     /** 岗位 */
+    //被注释的元素必须符合执行的正则表达式
     @Pattern(regexp = "^岗位:.*$")
     private String post;
 
     /** 邮箱 */
+    //被注释的元素必须是电子邮件地址
     @Email
     private String email;
 
     /** 头像 */
+    //被注释的元素必须的是一个url地址
     @URL
     private String portrait;
 
     /** 爱好 */
+    //自定义注解
     @Hobby
     private String hobby;
 
